e9d3caffbfd132768402c540f2ce9965e63421e3
Emmalie Dion <emmalie@uoregon.edu>
2016-05-16 20:28:02 -0700

    working on external id page with connections

--- 423GUI/src/pkg423gui/TestClientReg.java
+++ 423GUI/src/pkg423gui/TestClientReg.java
@@ -38,7 +38,7 @@ import sun.misc.BASE64Encoder;
  */
 public class TestClientReg extends javax.swing.JFrame implements ActionListener {
 
-    ArrayList<String> pictures = new ArrayList<String>();
+    ArrayList<String> pictures = new ArrayList<>();
     int count = 0;
     static String images = "";
     static Webcam webcam = Webcam.getDefault();
@@ -70,11 +70,9 @@ public class TestClientReg extends javax.swing.JFrame implements ActionListener 
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
         jTextField2 = new javax.swing.JTextField();
-        jTextField3 = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
         jLabel5 = new javax.swing.JLabel();
         jButton2 = new javax.swing.JButton();
@@ -89,15 +87,11 @@ public class TestClientReg extends javax.swing.JFrame implements ActionListener 
 
         jLabel2.setText("email");
 
-        jLabel3.setText("password");
-
         jLabel4.setText("Gender");
 
         jTextField1.setText("John Doe");
 
         jTextField2.setText("johndoe@gmail.com");
-
-        jTextField3.setText("jdoe14");
 
         jButton1.setText("Sign Up");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -142,14 +136,12 @@ public class TestClientReg extends javax.swing.JFrame implements ActionListener 
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel1)
                             .addComponent(jLabel2)
-                            .addComponent(jLabel3)
                             .addComponent(jLabel4))
-                        .addGap(49, 49, 49)
+                        .addGap(60, 60, 60)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                                 .addComponent(jTextField2, javax.swing.GroupLayout.DEFAULT_SIZE, 113, Short.MAX_VALUE)
-                                .addComponent(jTextField1)
-                                .addComponent(jTextField3))
+                                .addComponent(jTextField1))
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addComponent(jRadioButton1)
                                 .addGap(10, 10, 10)
@@ -167,11 +159,7 @@ public class TestClientReg extends javax.swing.JFrame implements ActionListener 
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel2)
                     .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel3)
-                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(33, 33, 33)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4)
                     .addComponent(jRadioButton1)
@@ -263,27 +251,47 @@ public class TestClientReg extends javax.swing.JFrame implements ActionListener 
         System.out.println(pictures);
     }
     
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    public String[] getInfo(){
+        
+        String info[] = new String[3];
         
         String name = jTextField1.getText();
-        System.out.println(name);
+        info[0] = name;
+        //System.out.println(name);
         String email = jTextField2.getText();
-        System.out.println(email);
-        String password = jTextField3.getText();
-        System.out.println(password);
+        info[1] = email;
+        //System.out.println(email);
         String gender = "";
         if(jRadioButton1.isSelected()){
             gender = jRadioButton1.getText();
-            System.out.println(gender);
+            info[2]=gender;
+            //System.out.println(gender);
         }
         if(jRadioButton2.isSelected()){
             gender = jRadioButton2.getText();
-            System.out.println(gender);
+            info[2] = gender;
+            //System.out.println(gender);
         }
         
+        return info;
+    }
+    
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        
+        String[] usr_info = getInfo();
+        ExtID.display(usr_info);
+        
+        close();
+        ExtID s = new ExtID();
+        s.setVisible(true);
+           
     }//GEN-LAST:event_jButton1ActionPerformed
 
-    
+    private void close() {
+        WindowEvent winClose = new WindowEvent(this,WindowEvent.WINDOW_CLOSING);
+        Toolkit.getDefaultToolkit().getSystemEventQueue().postEvent(winClose);
+        
+    }
     /**
      * @param args the command line arguments
      */
@@ -325,7 +333,6 @@ public class TestClientReg extends javax.swing.JFrame implements ActionListener 
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
@@ -333,7 +340,6 @@ public class TestClientReg extends javax.swing.JFrame implements ActionListener 
     private javax.swing.JRadioButton jRadioButton2;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
     // End of variables declaration//GEN-END:variables
 
     
