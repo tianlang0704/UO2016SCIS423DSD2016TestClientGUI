c3fe1bd3efbb561cb3d15ecd84985183b637d32b
bryonf <fleming_bryon@hotmail.com>
2016-05-18 01:05:43 -0700

    GUI backend still sparse; can't seem to find documentation regarding return messages from server. Still searching. Very late push due to internet problems.

--- 423GUI/src/pkg423guibackend/GUIBackend.java
+++ 423GUI/src/pkg423guibackend/GUIBackend.java
@@ -1,29 +1,33 @@
 package pkg423guibackend;
 
 import java.util.ArrayList;
-
 import dsd2016.api.*;
 
 public class GUIBackend {
 	
-	public static int[] verifyEmail(String email) {
+	public static int[] registerUser(ArrayList<String> inB64Pics, ArrayList<String> outB64BadPics) {
 		StringBuilder outMsg = new StringBuilder();
-		int[] codes = new int[1];
-		codes[0] = DSD2016JAVA.verifyEmail(email, outMsg) ? GUIConstants.SUCCESS : GUIConstants.FAILURE;
+		int[] codes = new int[2];
+                int result = DSD2016JAVA.registerNewUser(inB64Pics, outB64BadPics, outMsg);
+                codes[1] = result == DSD2016JAVA.ERRORCODE_REGISTER_FAIL ? GUIConstants.FAILURE
+                        : result == DSD2016JAVA.ERRORCODE_REGISTER_IO_ERROR ? GUIConstants.IO_ERROR
+                        : result == DSD2016JAVA.ERRORCODE_REGISTER_UNKNOWN ? GUIConstants.UNKNOWN_ERROR
+                        : GUIConstants.SUCCESS;
+		codes[0] = codes[1] == GUIConstants.SUCCESS ? GUIConstants.SUCCESS : GUIConstants.FAILURE;
+                
 		return codes;
 	}
 	
-	public static int[] registerUser(String name, String email, String password, String gender, ArrayList<String> inB64Pics, ArrayList<String> outB64BadPics) {
+	public static int[] validateUser(String userID, String inB64Pic) {
 		StringBuilder outMsg = new StringBuilder();
-		int[] codes = new int[1];
-		codes[0] = DSD2016JAVA.registerNewUser(name, email, password, gender, inB64Pics, outB64BadPics, outMsg) == 1 ? GUIConstants.SUCCESS : GUIConstants.FAILURE;
-		return codes;
-	}
-	
-	public static int[] validateUser(String email, String password, String inB64Pic) {
-		StringBuilder outMsg = new StringBuilder();
-		int[] codes = new int[1];
-		codes[0] = DSD2016JAVA.validateUser(email, password, inB64Pic, outMsg) == 1 ? GUIConstants.SUCCESS : GUIConstants.FAILURE;
+		int[] codes = new int[2];
+                int result = DSD2016JAVA.validateUser(userID, inB64Pic, outMsg);
+                codes[1] = result == DSD2016JAVA.ERRORCODE_LOGIN_FAIL ? GUIConstants.FAILURE
+                        : result == DSD2016JAVA.ERRORCODE_LOGIN_IO_ERROR ? GUIConstants.IO_ERROR
+                        : result == DSD2016JAVA.ERRORCODE_LOGIN_UNKNOWN ? GUIConstants.UNKNOWN_ERROR
+                        : GUIConstants.SUCCESS;
+                codes[0] = codes[1] == GUIConstants.SUCCESS ? GUIConstants.SUCCESS : GUIConstants.FAILURE;
+                
 		return codes;
 	}
 }
--- 423GUI/src/pkg423guibackend/GUIConstants.java
+++ 423GUI/src/pkg423guibackend/GUIConstants.java
@@ -1,7 +1,8 @@
 package pkg423guibackend;
 
 public class GUIConstants {
-	public static final int SUCCESS = 0;
-	public static final int FAILURE = 1;
-	//Add more in later iterations
+	public static final int SUCCESS = 1;
+	public static final int FAILURE = 0;
+	public static final int IO_ERROR = -1;
+	public static final int UNKNOWN_ERROR = -2;
 }
